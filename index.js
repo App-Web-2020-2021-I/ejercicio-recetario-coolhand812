351e092c16591e3c352fe4df8a83be67e55d59a3
Joe Diaz <Coolhand812@alumnos.uaslp.edu.mx>
2020-10-16 23:24:34 -0500

    added hide/show code for col1

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    $("#button-hideCol1").click(function(){
+        $("#myCol1").hide(2000);
+    });
+
+    $("#button-hideCol1").click(function(){
+        $("#myCol1").show(2000);
+    });
 });
